7828db65ca94b27182bc47b85cf653de6c399d8e
Teknicallity <joshua.s.3322@gmail.com>
2024-11-20 12:04:13 -0500

    Add linear regressions

--- main.R
+++ main.R
@@ -2,3 +2,11 @@ library(readr)
 laptop_prices_modified <- read_csv("laptop_prices_modified.csv")
 View(laptop_prices_modified)
 
+subset_columns <- c("inches", "ram", "weight", "storageamount", "storagetype") # "company"
+
+for (col in subset_columns) {
+  print(toupper(col))
+  formula <- as.formula(paste("price_euros ~", col))
+  model <- lm(formula, data = laptop_prices_modified)
+  print(summary(model))
+}
