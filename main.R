122dfc05bfecb0fae105cddb7b1729a05e9d821a
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-11-27 10:42:34 -0500

    Added sqrt transformation

--- main.R
+++ main.R
@@ -39,3 +39,19 @@ for (col in subset_columns) {
   plot(model, main = paste("Diagnostics for", toupper(col)))
 }
 
+# Apply square root transformation to the relevant columns and then fit the models
+for (col in subset_columns) {
+  print(toupper(col)) 
+  transformed_data <- laptop_prices_modified
+  transformed_data$sqrt_price <- sqrt(transformed_data$price_euros)
+  if (is.numeric(transformed_data[[col]])) {
+    transformed_data[[paste0("sqrt_", col)]] <- sqrt(transformed_data[[col]])
+  }
+  formula <- as.formula(paste("sqrt_price ~", paste0("sqrt_", col)))
+  model <- lm(formula, data = transformed_data)
+  fitted_values <- fitted(model)
+  residuals_values <- residuals(model)
+  par(mfrow = c(2, 2))  # Arrange the plot layout (2x2 grid)
+  plot(model, main = paste("Diagnostics for", toupper(col)))
+}
+
