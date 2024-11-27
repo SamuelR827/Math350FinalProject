f00b2380ce11926d672134223cac25543f23247f
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-11-27 10:18:06 -0500

    Added descriptive statistics

--- main.R
+++ main.R
@@ -10,3 +10,25 @@ for (col in subset_columns) {
   model <- lm(formula, data = laptop_prices_modified)
   print(summary(model))
 }
+
+# Descriptive statistics
+for (col in subset_columns) {
+  print(toupper(col)) 
+  column_data <- laptop_prices_modified[[col]]
+  if (is.numeric(column_data)) {
+    stats <- c(
+      Mean = mean(column_data, na.rm = TRUE),
+      Median = median(column_data, na.rm = TRUE),
+      SD = sd(column_data, na.rm = TRUE),
+      Min = min(column_data, na.rm = TRUE),
+      Max = max(column_data, na.rm = TRUE)
+    )
+    print(stats)
+  } else if (is.factor(column_data) || is.character(column_data)) {
+    print(table(column_data))
+  } else {
+    print("Unsupported data type")
+  }
+}
+
+#
