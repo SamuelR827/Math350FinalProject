8af457b5954bdf711a4849a37d01a7c2c78a1606
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-11-27 10:22:57 -0500

    Remove redundant descriptive statistics

--- main.R
+++ main.R
@@ -4,31 +4,12 @@ View(laptop_prices_modified)
 
 subset_columns <- c("inches", "ram", "weight", "storageamount", "storagetype") # "company"
 
+# Summarize data
 for (col in subset_columns) {
   print(toupper(col))
   formula <- as.formula(paste("price_euros ~", col))
   model <- lm(formula, data = laptop_prices_modified)
   print(summary(model))
-}
-
-# Descriptive statistics
-for (col in subset_columns) {
-  print(toupper(col)) 
-  column_data <- laptop_prices_modified[[col]]
-  if (is.numeric(column_data)) {
-    stats <- c(
-      Mean = mean(column_data, na.rm = TRUE),
-      Median = median(column_data, na.rm = TRUE),
-      SD = sd(column_data, na.rm = TRUE),
-      Min = min(column_data, na.rm = TRUE),
-      Max = max(column_data, na.rm = TRUE)
-    )
-    print(stats)
-  } else if (is.factor(column_data) || is.character(column_data)) {
-    print(table(column_data))
-  } else {
-    print("Unsupported data type")
-  }
 }
 
 # Generate Diagnostics Plots
