b09b20a6e649b5f6db2e9cd8d36f9b963dc7f273
AmbivalentLeather <144703073+AmbivalentLeather@users.noreply.github.com>
2024-11-27 10:37:37 -0500

    Add log remedial measures

--- main.R
+++ main.R
@@ -12,7 +12,7 @@ for (col in subset_columns) {
   print(summary(model))
 }
 
-# Generate Diagnostics Plots
+# Generate Diagnostics Plots -- No remedial measures
 for (col in subset_columns) {
   print(toupper(col)) 
   formula <- as.formula(paste("price_euros ~", col))
@@ -20,3 +20,22 @@ for (col in subset_columns) {
   par(mfrow = c(2, 2))
   plot(model, main = paste("Diagnostics for", toupper(col)))
 }
+
+# Apply logarithmic transformation to the relevant columns and then fit the models
+for (col in subset_columns) {
+  print(toupper(col)) 
+  transformed_data <- laptop_prices_modified
+  transformed_data$log_price <- log(transformed_data$price_euros)
+  if (is.numeric(transformed_data[[col]])) {
+    transformed_data[[paste0("log_", col)]] <- log(transformed_data[[col]])
+  }
+  formula <- as.formula(paste("log_price ~", paste0("log_", col)))
+  model <- lm(formula, data = transformed_data)
+  fitted_values <- fitted(model)
+  residuals_values <- residuals(model)
+  
+  # Generate diagnostics plots
+  par(mfrow = c(2, 2))
+  plot(model, main = paste("Diagnostics for", toupper(col)))
+}
+
